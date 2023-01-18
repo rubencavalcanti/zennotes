2d0ee7571451f01c71f87a9a6949e758c17433d2
rubencavalcanti <rubencavalcanti@gmail.com>
2023-01-17 23:42:15 -0300

    criação de ambiente

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+//view engine
+app.set('view engine', 'ejs');
+
+//static
+app.use(express.static('public'));
+
+
+//body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//rotas
+
+//renderizar home
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+//rodar o servidor
+app.listen(3000, () => {
+    console.log("O servidor está rodando!")
+})
