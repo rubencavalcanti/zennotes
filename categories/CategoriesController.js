3bd048a7301113e4b0260fe891502fc9f5cbdf3d
rubencavalcanti <rubencavalcanti@gmail.com>
2023-01-19 00:01:38 -0300

    rota para criação de categiria

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const slugiFy = require('slugify');
+//models
+const Category = require('./Category');
 
+
+//rotas
 router.get('/admin/categories', (req, res) => {
     res.render('admin/categories/index')
+})
+
+//renderizar view new
+router.get('/admin/categories/new', (req, res) => {
+    res.render('admin/categories/new')
+})
+
+
+router.post('/admin/categories/create', (req, res) => {
+    let title = req.body.title;
+    if(title != undefined) {
+        Category.create({
+            title: title,
+            slug: slugiFy(title)
+        }).then(() => res.redirect('/admin/categories/'))
+    } else {
+        res.render('admin/categories/new')
+    }
+    
 })
 
 module.exports = router;
