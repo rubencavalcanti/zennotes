78bf69e028806052f88a6bf92db5299fe9a6f3c7
rubencavalcanti <rubencavalcanti@gmail.com>
2023-01-19 23:22:24 -0300

    rota para deletar categoria

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -46,6 +46,7 @@ router.get('/admin/categories/edit/:id', (req, res) => {
     })
 })
 
+//rota para realizar o update
 router.post('/admin/categories/update', (req, res) => {
     let id = req.body.id;
     let title = req.body.title;
@@ -60,6 +61,24 @@ router.post('/admin/categories/update', (req, res) => {
     }).then(() => {
         res.redirect('/admin/categories')
     })
+})
+
+
+//rota para deletar categoria
+router.post('/admin/categories/delete', (req, res) => {
+    let id = req.body.id;
+
+    if (id != undefined && !isNaN(id)){
+        Category.destroy({
+            where: {id: id},
+        }).then(() => {
+            res.json({
+                "sucess" : "success"
+            })
+        })
+    } else {
+        res.redirect('/admin/categories')
+    }
 })
 
 
